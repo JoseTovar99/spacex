10d7e8e9d2ac2e9df9ac6f7de5f1d9e21ebfc92d
JoseTovar510 <42582683+JoseTovar510@users.noreply.github.com>
2021-05-19 14:24:14 -0700

    Added Files

---
+++ script.js
@@ -0,0 +1,24 @@
+// ScrollMagic is the scrolling effect
+//Timeline to makes sure there's a specific time to scroll
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+.to('.rock', 2, {y: -300})
+.to('.girl', 3, {y: -200}, '-=3')
+.fromTo('.bg1', 3, {y: -50}, {y: 0, duration: 3}, '-=3')
+.to('.content', 3, {top: '0%',}, '-=3')
+.fromTo('.content-images', {opacity: 0}, {opacity: 1, duration: 3})
+.fromTo('.text', {opacity: 0}, {opacity: 1, duration: 3})
+
+
+let scene = new ScrollMagic.Scene({
+  // Section will be instant
+  triggerElement: "section",
+  duration: "140%",
+  triggerHook: 0,
+})
+
+.setTween(timeline)
+.setPin("section")
+.addTo(controller);
